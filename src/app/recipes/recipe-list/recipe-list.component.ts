131407928ff2d8c622a73f18e3846c8a17987a11
Eemeli Surakka <eemeli.surakka@hotmail.fi>
2020-11-08 16:33:43 +0100

    54. Recipe details

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -9,6 +9,7 @@ import { Recipe } from '../recipe.model';
 export class RecipeListComponent implements OnInit {
   recipes: Recipe[] = [
     new Recipe('Test recipe', 'Test recipe description for an incoming delicious recipe.', 'https://cdn.pixabay.com/photo/2015/08/25/03/50/background-906135_1280.jpg'),
+    new Recipe('Test recipe', 'Test recipe description for an incoming delicious recipe.', 'https://cdn.pixabay.com/photo/2015/08/25/03/50/background-906135_1280.jpg'),
   ];
 
   constructor() { }
