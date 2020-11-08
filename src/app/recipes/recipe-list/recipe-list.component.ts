830c848d8dddb692bb70e698a1294a557f693fca
Eemeli Surakka <eemeli.surakka@hotmail.fi>
2020-11-08 15:53:52 +0100

    53. recipelist

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,6 +7,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe('Test recipe', 'Test recipe description for an incoming delicious recipe.', 'https://cdn.pixabay.com/photo/2015/08/25/03/50/background-906135_1280.jpg'),
+  ];
 
   constructor() { }
 
